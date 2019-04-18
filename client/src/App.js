d252def93e258af76cc97883ec274f1b4a671de5
akatsuki <AkafreakingBox>
2019-04-19 02:59:47 +0300

    adding a component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Personcomponent from "./component/person";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            React frontend powered by akabox
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>this is title </h1>
+                <Personcomponent/>
+                <Personcomponent/>
+                <Personcomponent/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/component/person.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Personcomponent extends Component {
+    render() {
+        return (
+            <div>
+                this is person component
+            </div>
+        );
+    }
+}
+
+export default Personcomponent;
