cec00b1a51ef726aa4e61b07284bae46d598bc4b
akatsuki <AkafreakingBox>
2019-04-19 03:24:20 +0300

    frontend puting a component and a func

--- client/src/component/person.js
+++ client/src/component/person.js
@@ -2,9 +2,16 @@ import React, {Component} from 'react';
 
 class Personcomponent extends Component {
     render() {
+        const randomnum = () => {
+            let testfuncret = Math.floor(Math.random()*100);
+            return testfuncret;
+        }
         return (
             <div>
                 this is person component
+                <br/>
+                {randomnum()}
+
             </div>
         );
     }
