d0b004c35a8a4028bc908c5eb036495ef591ba78
akatsuki <AkafreakingBox>
2019-04-19 03:42:31 +0300

    loading state

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 import Personcomponent from "./component/person";
 
 class App extends Component {
+    state = {
+        persons: [
+            {
+                name: 'somename',
+                age: 10
+            }, {
+                name: 'person2',
+                age: 11
+            }
+        ]
+    };
+
     render() {
         return (
             <div className="App">
+                <button>a button</button>
                 <h1>this is title </h1>
                 <Personcomponent name="aka" age="10"/>
                 <br/>
                 <Personcomponent>children text (can be html strucutre ) </Personcomponent>
                 <br/>
-                <Personcomponent/>
+                <Personcomponent name={this.state.persons[0].name} age={this.state.persons[0].age}/>
             </div>
         )
             ;
