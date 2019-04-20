38bca7b16952524fa0ce65c510bbd30c9d978c1e
akatsuki <AkafreakingBox>
2019-04-20 19:43:05 +0300

    buildcontrol css

---
+++ client/src/components/burger/buildcontrol/buildcontrol.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Buildcontrol extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Buildcontrol;
