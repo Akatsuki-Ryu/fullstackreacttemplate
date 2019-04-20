429f866f1ce70702cb8058d2292d2bc49de9b9ea
akatsuki <AkafreakingBox>
2019-04-20 15:36:13 +0300

    code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            React frontend powered by akabox
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>this is something </div>
+        );
+    }
 }
 
 export default App;
+
