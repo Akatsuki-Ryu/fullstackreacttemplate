dd156c741e41048b2e9669b1a8ca3f513ba3177c
akatsuki <AkafreakingBox>
2019-04-20 18:38:50 +0300

    burger contents

--- client/src/components/burger/burger.js
+++ client/src/components/burger/burger.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import './burger.css';
+import './burgeringre/burgeringre.css';
+import Burgeringre from "./burgeringre/burgeringre";
+
 
 class Burger extends Component {
     render() {
         return (
-            <div>
-                burger componnet
+            <div className={"burger"}>
+                <Burgeringre type={"bread-top"}/>
+                <Burgeringre type={"cheese"}/>
+                <Burgeringre type={"meat"}/>
+                <Burgeringre type={"bread-bottom"}/>
+
             </div>
         );
     }
