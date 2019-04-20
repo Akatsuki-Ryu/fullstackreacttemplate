2214f786c26b01a7e30b42b7d7487b8c56d7fbca
akatsuki <AkafreakingBox>
2019-04-20 19:31:21 +0300

    adding dynamic contents

--- client/src/components/burger/burger.js
+++ client/src/components/burger/burger.js
@@ -6,12 +6,16 @@ import Burgeringre from "./burgeringre/burgeringre";
 
 class Burger extends Component {
     render() {
+        //put ingredient array into div elements
+        console.log(Object.keys(this.props.ingredients));
         const transformedingredients = Object.keys(this.props.ingredients)
             .map(igkey => {
+                console.log(igkey);
                 return [...Array(this.props.ingredients[igkey])].map((_, i) => {
                     return <Burgeringre key={igkey + 1} type={igkey}/>
                 })
             })
+
         return (
             <div className={"burger"}>
                 <Burgeringre type={"bread-top"}/>
