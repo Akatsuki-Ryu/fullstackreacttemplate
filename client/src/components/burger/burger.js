abf406de051e648c1602cf610e1b782fdcd7d26c
akatsuki <AkafreakingBox>
2019-04-20 19:42:02 +0300

    when the array is empty , adding logic to it

--- client/src/components/burger/burger.js
+++ client/src/components/burger/burger.js
@@ -8,13 +8,20 @@ class Burger extends Component {
     render() {
         //put ingredient array into div elements
         console.log(Object.keys(this.props.ingredients));
-        const transformedingredients = Object.keys(this.props.ingredients)
+        let transformedingredients = Object.keys(this.props.ingredients)
             .map(igkey => {
-                console.log(igkey);
                 return [...Array(this.props.ingredients[igkey])].map((_, i) => {
-                    return <Burgeringre key={igkey + 1} type={igkey}/>
+                    return <Burgeringre key={igkey + i} type={igkey}/>
                 })
             })
+            .reduce((arr, el) => {
+                return arr.concat(el)
+            }, []);
+        if (transformedingredients.length === 0) {
+            transformedingredients = <p>please start adding stuff </p>
+        }
+        console.log(transformedingredients);
+
 
         return (
             <div className={"burger"}>
