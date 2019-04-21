7521bc67827b955193c04bb15114095abff5f0a9
akatsuki <AkafreakingBox>
2019-04-21 04:15:45 +0300

    toolbar css

---
+++ client/src/components/navigation/toolbar/toolbar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Toolbarcomp extends Component {
+    render() {
+        return (
+            <header>
+                <div>menu</div>
+                <div>logo</div>
+                <nav>
+                    <ul>links</ul>
+                </nav>
+            </header>
+        );
+    }
+}
+
+export default Toolbarcomp;
