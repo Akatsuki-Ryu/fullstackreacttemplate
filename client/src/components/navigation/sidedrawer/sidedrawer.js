12b69a078563b25f770f33ca4dc6867a8e67d415
akatsuki <AkafreakingBox>
2019-04-21 17:48:49 +0300

    sidedraw for logo

--- client/src/components/navigation/sidedrawer/sidedrawer.js
+++ client/src/components/navigation/sidedrawer/sidedrawer.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 import Logocomp from "../../logo/logo";
 import Navilist from "../navilist/navilist";
 import Naviitem from "../navilist/naviitem/naviitem";
+import './sidedrawer.css'
 
 class Sidedrawer extends Component {
     render() {
         return (
-            <div>
-                <Logocomp/>
-                <Naviitem link="/" active>burger builder</Naviitem>
-                <Naviitem link="/">checkout</Naviitem>
+            <div className={"sidedraw"}>
+                <div className={"logosidedrawer"}>
+                <Logocomp attr="something"/></div>
+                <div></div>
+              <Navilist/>
             </div>
         );
     }
