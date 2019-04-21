03eb7d086a0d48c7058fe73832be2b9f1d07e193
akatsuki <AkafreakingBox>
2019-04-21 03:33:14 +0300

    backdrop component

---
+++ client/src/components/UI/backdrop/backdrop.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Backdrop extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Backdrop;
