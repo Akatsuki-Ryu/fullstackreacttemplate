5264c274ed30d2e4d97b1d214f82822561baee77
akatsuki <AkafreakingBox>
2019-04-21 04:31:43 +0300

    navigation items css

---
+++ client/src/components/navigation/navilist/naviitem/naviitem.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Naviitem extends Component {
+    render() {
+        return (
+            <li><a href="/">a link</a></li>
+        );
+    }
+}
+
+export default Naviitem;
---
+++ client/src/components/navigation/navilist/navilist.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Naviitem from "./naviitem/naviitem";
+
+class Navilist extends Component {
+    render() {
+        return (
+            <ul>
+                <Naviitem/>
+            </ul>
+        );
+    }
+}
+
+export default Navilist;
