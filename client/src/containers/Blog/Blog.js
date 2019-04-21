a5a0cbc9796a06d639fa493ef88fbb5e1a89d34b
akatsuki <AkafreakingBox>
2019-04-21 21:07:30 +0300

    getting from rest api

--- client/src/containers/Blog/Blog.js
+++ client/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Postcomp';
 import FullwidthPost from '../../components/FullPost/FullwidthPost';
@@ -6,6 +7,13 @@ import NewPostcomp from '../../components/NewPost/NewPostcomp';
 import './Blog.css';
 
 class Blog extends Component {
+    componentDidMount() {
+        const post = axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(res=>{
+                console.log(res);
+            })
+    }
+
     render () {
         return (
             <div>
