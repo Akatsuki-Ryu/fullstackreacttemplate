8c03f0961ea4fabbc66c79004222f97f6cf6daaf
akatsuki <AkafreakingBox>
2019-04-21 04:28:03 +0300

    navigation items js

---
+++ client/src/components/navigation/naviitems/naviitems.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Naviitems extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Naviitems;
