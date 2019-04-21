39059bc961185653aa6520b2d0d3dd4384afb604
akatsuki <AkafreakingBox>
2019-04-21 04:21:18 +0300

    logo css and js

---
+++ client/src/components/logo/logo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Logocomp extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Logocomp;
